9bc4640c1c0271eadb79f2fc735e312de3e011c6
Dx724 <dx72dx@gmail.com>
2021-03-25 05:04:13 -0400

    Add works of art, header generation

--- Artwork/convert_to_polar.py
+++ Artwork/convert_to_polar.py
@@ -1,29 +1,42 @@
 from PIL import Image
 import math
 
-p = Image.open("mona_lisa.png")
+p = Image.open("The-Starry-Night.jpg")
 
 w = p.width
 h = p.height
 
 center = (w/2, h/2)
 
-NUM_POINTS = 500
+NUM_POINTS = 1500
 
-dist = 0.95
-print("led_off();")
-is_first = True
-while dist > 0.05:
-    for ang in range(-180, 181, 5):
-        polar_coord = (dist, ang)
-        px = round(dist * w / 2 * math.cos(math.radians(ang)))
-        py = round(dist * w / 2 * math.sin(math.radians(ang)))
-        color = p.getpixel((px, py))
-        print("do_arc({:.2f}, {}, {}, {}, {});".format(*polar_coord, *color))
-        if is_first:
-            print("led_on();")
-            is_first = False
-        dist -= (1/NUM_POINTS)*(0.95 - 0.05)
-        if dist <= 0.05:
-            break
-    
+data_r = []
+data_a = []
+data_c = []
+
+with open("artwork.h", "w+") as f:
+    dist = 0.95
+    f.write("void do_arc(double rad, double ang, byte r, byte g, byte b);\n")
+    f.write("void led_off();\n\n")
+    f.write("void do_artwork() {\n")
+    while dist > 0.05:
+        f.write("\tled_off();\n")
+        for ang in range(-180, 181, 4):
+            polar_coord = (dist, ang)
+            px = round(w/2 + dist * w / 2 * math.cos(math.radians(ang)))
+            py = round(w/2 + dist * w / 2 * math.sin(math.radians(ang)))
+            color = p.getpixel((px, py))
+            f.write("\tdo_arc({:.2f}, {}, {}, {}, {});\n".format(*polar_coord, *color))
+            data_r.append(dist)
+            data_a.append(ang*math.pi/180)
+            data_c.append((color[0]/255, color[1]/255, color[2]/255))
+            dist -= (1/NUM_POINTS)*(0.95 - 0.05)
+            if dist <= 0.05:
+                break
+    f.write("}\n")
+
+import matplotlib.pyplot as plt
+
+ax = plt.subplot(111, projection='polar')
+ax.scatter(data_a, data_r, c=data_c)
+plt.show()
--- README_tmp.txt
+++ README_tmp.txt
@@ -4,3 +4,12 @@ Stepper library (built in)
 1 mF capacitor
 
 15:2, 2:32, 4: 33
+
+-45.44  -12.38
+-53.54  24.81
+-45.44  -12.38
+-53.54  24.81
+-45.44  -12.38
+-53.54  24.81
+
+Spiral instead of circle! Why think only in terms of traditional angular movement?
