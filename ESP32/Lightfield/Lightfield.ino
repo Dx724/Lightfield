c83cf37c4f72ce53dd5df36e1ffc740bc9516b5e
Dx724 <dx72dx@gmail.com>
2021-03-25 00:49:54 -0400

    Implement polar coordinate system

--- ESP32/Lightfield/Lightfield.ino
+++ ESP32/Lightfield/Lightfield.ino
@@ -16,9 +16,10 @@ int blue = 255;
 
 double r1 = 8.89; // cm
 double r2 = 9.85; // cm
-double maxrad = r1 + r2 - 0.1 // cm (small tolerance provided)
+double maxrad = r1 + r2 - 0.1; // cm (small tolerance provided)
 
 void setup() {
+  Serial.begin(9600);
   stepper.setSpeed(15);
 
   servo.setPeriodHertz(50);
@@ -35,8 +36,8 @@ void stepper_move(int steps) {
   stepper.step(steps);
 }
 
-void stepper_toangle(int angle) {
-  stepper_move(stepsPerRevolution*(angle/360.0) - currentRotation);
+void stepper_to_angle(double angle) { // Calculated with acos, so should remain bounded
+  stepper_move((int) (stepsPerRevolution*(angle/360.0) - currentRotation));
 }
 
 void setColor(byte r, byte g, byte b) {
@@ -46,13 +47,23 @@ void setColor(byte r, byte g, byte b) {
 }
 
 void move_to_polar(double rad, double ang) {
-  
+  rad *= maxrad;
+  double theta1 = ang - acos((r1*r1+rad*rad-r2*r2)/(2*r1*rad));
+  double theta2 = acos((r1*r1+r2*r2-rad*rad)/(2*r1*r2));
+  theta1 *= 180.0 / PI;
+  theta2 *= 180.0 / PI;
+  stepper_to_angle(theta1);
+  servo.write((int)theta2);
+  Serial.print(theta1);
+  Serial.print("  ");
+  Serial.println(theta2);
 }
 
 void loop() {
-  stepper_toangle(45);
-  servo.write(45);
-  stepper_toangle(0);
-  servo.write(0);
-  setColor(red, green, blue);
+  move_to_polar(0.5, 20);\
+  setColor(255, 0, 0);
+  delay(1000);
+  move_to_polar(0.2, 40);
+  setColor(0, 0, 255);
+  delay(1000);
 }
